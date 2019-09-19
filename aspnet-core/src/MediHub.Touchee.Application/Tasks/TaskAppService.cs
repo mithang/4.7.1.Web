615abdbc1075f4bfa19ad23cc3f46a9f12e8e4f0
apple <minh.tran@medihub.com.vn>
2019-09-19 17:00:38 +0700

    demo unit of work

--- aspnet-core/src/MediHub.Touchee.Application/Tasks/TaskAppService.cs
+++ aspnet-core/src/MediHub.Touchee.Application/Tasks/TaskAppService.cs
@@ -5,15 +5,26 @@ using Abp.Application.Services;
 using Abp.Application.Services.Dto;
 using Abp.Collections.Extensions;
 using Abp.Domain.Repositories;
+using Abp.Domain.Uow;
+using MediHub.Touchee.Products;
+using MediHub.Touchee.Products.Dto;
 
 namespace MediHub.Touchee.Tasks
 {
     public class TaskAppService : AsyncCrudAppService<Task, TaskDto, int, GetAllTasksInput, CreateTaskInput, UpdateTaskInput>
     {
-        public TaskAppService(IRepository<Task> repository)
+        private readonly IUnitOfWorkManager _unitOfWorkManager;
+        private readonly IRepository<Product> _productRepository;
+
+        private readonly IRepository<Task> _taskRepository;
+
+        public TaskAppService(IRepository<Task> repository, IUnitOfWorkManager unitOfWorkManager, IRepository<Product> productRepository)
             : base(repository)
         {
             //CreatePermissionName = "MyTaskCreationPermission";
+            _unitOfWorkManager = unitOfWorkManager;
+            _productRepository = productRepository;
+            _taskRepository = repository;
         }
 
         protected override IQueryable<Task> CreateFilteredQuery(GetAllTasksInput input)
@@ -21,5 +32,70 @@ namespace MediHub.Touchee.Tasks
             return (System.Linq.IQueryable<MediHub.Touchee.Tasks.Task>)base.CreateFilteredQuery(input)
                 .WhereIf(input.Searchkey.Length > 0, t => t.Title == input.Searchkey);
         }
+
+        public void CreateTaskAndProduct(CreateProductDto input)
+        {
+            var product = new Product { Name = input.Name, Quantity = input.Quantity };
+            var task = new Task {
+                Title="T2",
+                Description="t2"
+            }; 
+
+            using (var unitOfWork = _unitOfWorkManager.Begin())
+            {
+                _productRepository.Insert(product);
+                //throw new Exception("loi"); Test Transaction
+                _taskRepository.Insert(task);
+
+                unitOfWork.Complete();
+            }
+        }
+
+        //[UnitOfWork]
+        //public SearchPeopleOutput SearchPeople(SearchPeopleInput input)
+        //{
+        //    // get IQueryable<Person>
+        //    var query = _personRepository.GetAll();
+
+        //    // add some filters if selected
+        //    if (!string.IsNullOrEmpty(input.SearchedName))
+        //    {
+        //        query = query.Where(person => person.Name.StartsWith(input.SearchedName));
+        //    }
+
+        //    if (input.IsActive.HasValue)
+        //    {
+        //        query = query.Where(person => person.IsActive == input.IsActive.Value);
+        //    }
+
+        //    // get paged result list
+        //    var people = query.Skip(input.SkipCount).Take(input.MaxResultCount).ToList();
+
+        //    return new SearchPeopleOutput { People = Mapper.Map<List<PersonDto>>(people) };
+        //}
+
+        //public void CreateTask(CreateTaskInput input)
+        //{
+        //    var task = new Task { Description = input.Description };
+
+        //    if (input.AssignedPersonId.HasValue)
+        //    {
+        //        task.AssignedPersonId = input.AssignedPersonId.Value;
+        //        _unitOfWorkManager.Current.Completed += (sender, args) => { /* TODO: Send email to assigned person */ };
+        //    }
+
+        //    _taskRepository.Insert(task);
+        //}
+
+        //public class SimpleTaskSystemCoreModule : AbpModule
+        //{
+        //    public override void PreInitialize()
+        //    {
+        //        Configuration.UnitOfWork.IsolationLevel = IsolationLevel.ReadCommitted;
+        //        Configuration.UnitOfWork.Timeout = TimeSpan.FromMinutes(30);
+        //    }
+
+        //    //...other module methods
+        //}
     }
 }
