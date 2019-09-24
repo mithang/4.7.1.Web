473888cc0e54386b040bc4c96803466228e5735f
apple <minh.tran@medihub.com.vn>
2019-09-24 21:49:28 +0700

    tao controller va view cho products

---
+++ aspnet-core/src/MediHub.Touchee.Web.Mvc/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Abp.AspNetCore.Mvc.Authorization;
+using Abp.AspNetCore.Mvc.Controllers;
+using MediHub.Touchee.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediHub.Touchee.Web.Controllers
+{
+    
+    public class ProductsController: AbpController
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [AbpMvcAuthorize(PermissionNames.Pages_Roles)]
+        public ActionResult CreateProduct()
+        {
+            return View();
+        }
+    }
+}
